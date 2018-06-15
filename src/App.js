3a726531189fd24ee7a1980ea65942b2e41a6aa6
Danny Lin <logicmelody@gmail.com>
2018-06-15 14:01:17 +0800

    Firbase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBP3qaxCCquSmECT6mR14G1BzLxuFVK-r0',
+      authDomain: 'auth-9dcbd.firebaseapp.com',
+      databaseURL: 'https://auth-9dcbd.firebaseio.com',
+      projectId: 'auth-9dcbd',
+      storageBucket: 'auth-9dcbd.appspot.com',
+      messagingSenderId: '484073493773'
+    });
+  }
 
   render() {
     return (
