412cfc531911cf660f609f6ebdfc86c32623d3be
Danny Lin <logicmelody@gmail.com>
2018-06-19 17:57:35 +0800

    Three states for login button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { loggedIn: null };
+
     firebase.initializeApp({
       apiKey: 'AIzaSyBP3qaxCCquSmECT6mR14G1BzLxuFVK-r0',
       authDomain: 'auth-9dcbd.firebaseapp.com',
@@ -16,13 +18,30 @@ class App extends Component {
       storageBucket: 'auth-9dcbd.appspot.com',
       messagingSenderId: '484073493773'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ loggedIn: user !== undefined });
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner style={{ alignSelf: 'center' }} size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
